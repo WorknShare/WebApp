d646ceeb01a9fc6b9bc675f14de2f1ca5276249f
SystemGlitch <jeremy.la@outlook.fr>
2018-02-13 12:39:40 +0100

    Employees migration

---
+++ database/migrations/2018_02_13_113230_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id_employee');
+            $table->string('email')->unique();
+            $table->string('name' , 25);
+            $table->string('surname', 25);
+            $table->char('phone', 10)->nullable();
+            $table->string('address');
+            $table->char('password', 60);
+            $table->string('role');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
