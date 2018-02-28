fe2fde9b5891ee94bc9b92a390387603b221c288
SystemGlitch <jeremy.la@outlook.fr>
2018-02-28 14:15:23 +0100

    Now correctly redirects admins when authenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,13 +17,10 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
         switch ($guard) {
         case 'admin':
           if (Auth::guard($guard)->check()) {
-            return redirect()->route('admin.dashboard');
+            return redirect()->route('admin.home');
           }
           break;
         default:
