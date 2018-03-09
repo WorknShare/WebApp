7bf222b0a494c932db2bbfc4ff1ea8574963a163
SystemGlitch <jeremy.la@outlook.fr>
2018-03-09 16:51:42 +0100

    Finished site test

--- tests/Feature/SiteTest.php
+++ tests/Feature/SiteTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use App\Employee;
 use App\User;
 use App\Site;
+use App\Schedule;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Auth;
@@ -144,6 +145,36 @@ class SiteTest extends TestCase
     }
 
     /**
+     * Test site show, mainly tests auth access
+     *
+     * @return void
+     */
+    public function testSiteShow()
+    {
+    	$siteShow = factory(Site::class)->create();
+    	$this->be($this->employees[1], 'admin');
+
+    	//Employee can see site details
+    	$this->get('admin/site/'. $siteShow->id_site)
+    		->assertStatus(200)
+    		->assertSeeText($siteShow->name)
+    		->assertSeeText($siteShow->address);
+
+		Auth::logout();
+        $this->be($this->user, 'web');
+
+        //Normal user can't see site details
+        $this->get('admin/site/'. $siteShow->id_site)->assertRedirect('admin/login');
+
+		Auth::logout();
+
+		//Unauthenticated user can't see site details
+		$this->get('admin/site/'. $siteShow->id_site)->assertRedirect('admin/login');
+
+    	//TODO access test
+    }
+
+    /**
      * Test site modify
      *
      * @return void
@@ -151,28 +182,111 @@ class SiteTest extends TestCase
     public function testSiteModify()
     {
     	$siteModify = factory(Site::class)->create();
+    	$siteModifyUnique = factory(Site::class)->create();
     	$this->be($this->employees[1], 'admin');
 
     	//Employee get edit form
-        $this->assertTrue(true);
+        $this->get('admin/site/'. $siteModify->id_site .'/edit')->assertStatus(200);
+
+        //Submit with invalid method
+        $this->post('/admin/site/'. $siteModify->id_site , array('name' => 'Sitename'))->assertStatus(405);
 
         //Submit invalid form
+        $this->put('/admin/site/'. $siteModify->id_site , array('name' => 'Sitename'))
+				->assertRedirect('admin/site/'. $siteModify->id_site .'/edit')
+				->assertSessionHasErrors();
+		session()->forget('errors');
+
+		$this->put('/admin/site/'. $siteModify->id_site , array('address' => '12 rue de l\'Avenue'))
+				->assertRedirect('admin/site/'. $siteModify->id_site .'/edit')
+				->assertSessionHasErrors();
+		session()->forget('errors');
+
+		$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'Sitename', 'wifi'=>1, 'drink'=>'1'))
+				->assertRedirect('admin/site/'. $siteModify->id_site .'/edit')
+				->assertSessionHasErrors();
+		session()->forget('errors');
+
+		$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'Sitename', 'fakeField'=>';DROP TABLE sites;'))
+				->assertRedirect('admin/site/'. $siteModify->id_site .'/edit')
+				->assertSessionHasErrors();
+		session()->forget('errors');
 
         //Submit valid form with extra field (SQL injection scenario)
+        $this->put('/admin/site/'. $siteModify->id_site , array('name' => 'InjectionModify', 'address' => '12 rue de la modification', 'fakeField'=>';DROP TABLE sites;'))
+				->assertRedirect('/admin/site/'. $siteModify->id_site)
+				->assertSessionHas('ok', 'Le site InjectionModify a été modifié.');
+		$this->assertDatabaseHas('sites', array('name' => 'InjectionModify', 'address' => '12 rue de la modification'));
+		session()->forget('ok');
+
+		$this->get('/admin/site/'. $siteModify->id_site)->assertStatus(200)->assertSeeText('InjectionModify');
+		$this->get('/admin/site')->assertStatus(200)->assertSeeText('InjectionModify');
 
         //Submit valid form
+        $this->put('/admin/site/'. $siteModify->id_site , array('name' => 'SitenameModify', 'address' => '13 rue de la modification', 'wifi'=>1))
+				->assertRedirect('/admin/site/'. $siteModify->id_site)
+				->assertSessionHas('ok', "Le site SitenameModify a été modifié.");
+        session()->forget('ok');
+        $this->assertDatabaseHas('sites', array('name' => 'SitenameModify', 'address' => '13 rue de la modification', 'wifi'=> 1, 'drink'=> 0));
 
-        //Employee can see the modification in the list
+        //Employee can see the modification in the list and on the show page
+        $this->get('/admin/site')->assertStatus(200)->assertSeeText('SitenameModify');
+        $this->get('/admin/site/'. $siteModify->id_site)->assertStatus(200)->assertSeeText('SitenameModify');
 
         //Uniqueness test
+        $this->get('admin/site/'. $siteModify->id_site .'/edit')->assertStatus(200);
+        $this->put('/admin/site/'. $siteModify->id_site , array('name' => $siteModifyUnique->name, 'address' => $siteModifyUnique->address, 'drink'=>1))
+				->assertRedirect('/admin/site/'. $siteModify->id_site .'/edit')
+				->assertSessionHasErrors();
+        session()->forget('errors');
+        $this->assertDatabaseMissing('sites', array('id_site' => $siteModify->id_site, 'name' => $siteModifyUnique->name, 'address' => $siteModifyUnique->address, 'wifi'=> 1, 'drink'=> 0));
+
+        $this->put('/admin/site/'. $siteModify->id_site , array('name' => 'UniqueModify', 'address' => $siteModifyUnique->address, 'drink'=>1))
+				->assertRedirect('/admin/site/'. $siteModify->id_site .'/edit')
+				->assertSessionHasErrors();
+        session()->forget('errors');
+        $this->assertDatabaseMissing('sites', array('name' => 'UniqueModify', 'address' => $siteModifyUnique->address));
+
+       	$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'UniqueModify', 'address' => '15 rue de l\'Avenue', 'drink'=>1))
+				->assertRedirect('/admin/site/'. $siteModify->id_site)
+				->assertSessionHas('ok', "Le site UniqueModify a été modifié.");
+        session()->forget('ok');
+        $this->assertDatabaseHas('sites', array('name' => 'UniqueModify', 'address' => '15 rue de l\'Avenue', 'wifi'=> 0, 'drink'=> 1));
+
+        Auth::logout();
+        $this->be($this->user, 'web');
+
+        //A normal user can't get the edit form
+        $this->get('admin/site/'. $siteModify->id_site .'/edit')->assertRedirect('admin/login');
+
+        //Submit invalid form
+		$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'SiteUser'))
+				->assertRedirect('/admin/login')
+				->assertSessionMissing('errors');
+
+        //Submit valid form
+		$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'SiteUser', 'address' => '20 rue de l\'Avenue', 'wifi'=>1))
+				->assertRedirect('/admin/login')
+				->assertSessionMissing('ok');
+        session()->forget('ok');
+        $this->assertDatabaseMissing('sites', array('name' => 'SiteUser', 'address' => '20 rue de l\'Avenue', 'wifi'=> 1, 'drink'=> 0));
 
         Auth::logout();
 
         //An unauthenticated user can't get the edit form
+        $this->get('admin/site/'. $siteModify->id_site .'/edit')->assertRedirect('admin/login');
 
         //Submit invalid form
+		$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'SiteUser'))
+				->assertRedirect('/admin/login')
+				->assertSessionMissing('errors');
 
         //Submit valid form
+		$this->put('/admin/site/'. $siteModify->id_site , array('name' => 'SiteUser', 'address' => '20 rue de l\'Avenue', 'wifi'=>1))
+				->assertRedirect('/admin/login')
+				->assertSessionMissing('ok');
+        session()->forget('ok');
+        $this->assertDatabaseMissing('sites', array('name' => 'SiteUser', 'address' => '20 rue de l\'Avenue', 'wifi'=> 1, 'drink'=> 0));
 
         //TODO access test
     }
@@ -184,7 +298,87 @@ class SiteTest extends TestCase
      */
     public function testSiteSchedule()
     {
-        $this->assertTrue(true);
+    	$siteSchedule = factory(Site::class)->create();
+        $this->be($this->employees[1], 'admin');
+
+        $this->get('admin/site/'. $siteSchedule->id_site)->assertStatus(200);
+
+        //Employee can create schedule
+        //Submit invalid form
+        $this->post('/schedule' , array('id_site' => $siteSchedule->id_site))
+				->assertRedirect('/admin/site/'. $siteSchedule->id_site)
+				->assertSessionHasErrors();
+		session()->forget('errors');
+
+		$this->post('/schedule' , array('id_site' => $siteSchedule->id_site, 'day'=>1, 'hour_opening'=> 'date', 'hour_closing' => '17:00'))
+				->assertRedirect('/admin/site/'. $siteSchedule->id_site)
+				->assertSessionHasErrors();
+		session()->forget('errors');
+
+		$this->post('/schedule' , array('id_site' => $siteSchedule->id_site, 'day'=>1, 'hour_opening'=> '18:00', 'hour_closing' => '17:00'))
+				->assertRedirect('/admin/site/'. $siteSchedule->id_site)
+				->assertSessionHasErrors();
+		session()->forget('errors');
+
+		for ($i = 0 ; $i < 7 ; $i++)
+		{ 
+		
+			//Submit valid form
+			$this->post('/schedule' , array('id_site' => $siteSchedule->id_site, 'day'=> $i, 'hour_opening'=> '0'.$i.':00', 'hour_closing' => ($i+10).':00'))
+				->assertRedirect('/admin/site/'. $siteSchedule->id_site)
+				->assertSessionHas('ok', 'L\'horaire a été créé.');
+			session()->forget('ok');
+			$this->assertDatabaseHas('site_schedules', array('day' => $i, 'hour_opening'=> '0'.$i.':00', 'hour_closing' => '0'.($i+10).':00'));
+
+        	//See schedule in list
+        	$this->get('admin/site/'. $siteSchedule->id_site)->assertSeeText($i.':00')->assertSeeText(($i+10).':00');
+
+        	//Delete the schedule
+    		$this->delete('schedule/'.($i+1))->assertRedirect('admin/site/'. $siteSchedule->id_site)->assertSessionHas('ok','L\'horaire a été supprimé.');
+    		session()->forget('ok');
+    		$this->assertDatabaseMissing('site_schedules', array('day' => $i, 'hour_opening'=> '0'.$i.':00', 'hour_closing' => '0'.($i+10).':00'));
+
+        	//Don't see the schedule anymore in the list
+    		$this->get('admin/site/'. $siteSchedule->id_site)->assertDontSeeText($i.':00')->assertDontSeeText(($i+10).':00');
+
+		}
+
+        Auth::logout();
+        $this->be($this->user, 'web');
+
+        //Normal user can't create nor destroy a schedule
+        //Submit invalid form
+        $this->post('/schedule' , array('id_site' => $siteSchedule->id_site))
+				->assertRedirect('/admin/login')
+				->assertSessionMissing('errors');
+
+		//Submit valid form
+		$this->post('/schedule' , array('id_site' => $siteSchedule->id_site, 'day'=> 2, 'hour_opening'=> '02:00', 'hour_closing' => '10:00'))
+			->assertRedirect('/admin/login')
+			->assertSessionMissing('ok');
+		session()->forget('ok');
+		$this->assertDatabaseMissing('site_schedules', array('day' => 2, 'hour_opening'=> '02:00', 'hour_closing' => '10:00'));
+
+		$schedule = factory(Schedule::class)->create(['id_site' => $siteSchedule->id_site]);
+		$this->delete('schedule/'. $schedule->id_schedule)->assertRedirect('admin/login')->assertSessionMissing('ok');
+
+        Auth::logout();
+
+        //Unauthenticated user can't create nor destroy a schedule
+        //Submit invalid form
+        $this->post('/schedule' , array('id_site' => $siteSchedule->id_site))
+				->assertRedirect('/admin/login')
+				->assertSessionMissing('errors');
+
+		//Submit valid form
+		$this->post('/schedule' , array('id_site' => $siteSchedule->id_site, 'day'=> 2, 'hour_opening'=> '02:00', 'hour_closing' => '10:00'))
+			->assertRedirect('/admin/login')
+			->assertSessionMissing('ok');
+		session()->forget('ok');
+		$this->assertDatabaseMissing('site_schedules', array('day' => 2, 'hour_opening'=> '02:00', 'hour_closing' => '10:00'));
+
+		$schedule = factory(Schedule::class)->create(['id_site' => $siteSchedule->id_site]);
+		$this->delete('schedule/'. $schedule->id_schedule)->assertRedirect('admin/login')->assertSessionMissing('ok');
 
         //TODO access test
     }
