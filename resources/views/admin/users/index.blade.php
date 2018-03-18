59d7c5fd72ee1062269742ed9572a2d0b4f28af6
Luis <valdez.jds.16@gmail.com>
2018-03-18 15:26:44 +0100

    backoffice users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -36,13 +36,34 @@ Clients
       <th>Email</th>
       <th>Nom</th>
       <th>Prénom</th>
+			<th>Compte actif</th>
+			<th style="width:30px"></th>
+			<th style="width:30px"></th>
     </tr>
     @foreach ($clients as $client)
     <tr>
     	<td>{{ $client->id_client }}</td>
-      <td><b><a href="{{ route('user.show', $client->id_client) }}">{{ $client->email }}</a></b></td>
+      <td><b><a href="{{ route('user.edit_admin', $client->id_client) }}">{{ $client->email }}</a></b></td>
       <td>{{ $client->name }}</td>
       <td>{{ $client->surname }}</td>
+			<td>{!! Html::badge(!$client->is_deleted) !!}</td>
+			@if (!$client->is_deleted)
+				<td><a class="point-cursor" href="{{ route('user.edit_admin', $client->id_client) }}"><i class="fa fa-pencil"></td>
+				<td>
+					{{ Form::open(['method' => 'DELETE', 'route' => ['user.ban', $client->id_client]]) }}
+			 			<a value="ban" type="submit" class="submitBanUser point-cursor text-danger"><i class="fa fa-gavel"></i></a>
+					{{ Form::close() }}
+				</td>
+			@else
+				<td></td>
+				<td>
+					{{ Form::open(['method' => 'put', 'route' => ['user.unban', $client->id_client]]) }}
+						<a value="unban" type="submit" class="submitUnbanUser point-cursor text-success"><i class="fa fa-gavel"></i></a>
+					{{ Form::close() }}
+				</td>
+			@endif
+
+
     </tr>
 	@endforeach
   </table>
@@ -60,4 +81,24 @@ Clients
 		{{ $links }}
 	</div>
 </div>
+@endsection
+
+
+@section('scripts')
+
+	<script type="text/javascript">
+	$(function() {
+		$('.submitBanUser').click(function() {
+			if(confirm('Voulez-vous vraiment bannir cet utilisateur ?'))
+				$(this).parent().submit();
+		});
+
+		$('.submitUnbanUser').click(function() {
+			if(confirm('Voulez-vous vraiment gracier cet utilisateur ?'))
+				$(this).parent().submit();
+		});
+	});
+
+	</script>
+
 @endsection
