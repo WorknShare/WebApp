163aeb7f923e61c3466e92dcb4f3687e87928364
SystemGlitch <jeremy.la@outlook.fr>
2018-03-22 10:14:17 +0100

    Navbar

--- resources/views/landing/welcome.blade.php
+++ resources/views/landing/welcome.blade.php
@@ -1,40 +1,8 @@
-@extends('layouts.landing')
+@extends('layouts.app_public')
 
 @section('content')
-    <header>
-        <nav class="navbar navbar-default navbar-fixed navbar-transparent dark white--outline-- bootsnav">
 
-            <div class="container">
-
-                <!-- Start Header Navigation -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <a class="navbar-brand" href="./index.html">
-                        <img src="{{ asset('img/banner64_2.png') }}" class="logo logo-display" alt="">
-                        <img src="{{ asset('img/banner64.png') }}" class="logo logo-scrolled" alt="">
-                    </a>
-                </div>
-                <!-- End Header Navigation -->
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="navbar-menu">
-                    <ul class="nav navbar-nav navbar-right" data-in="" data-out="">
-                        <li>
-                            <a href="{{ route('login') }}">Se connecter</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('register') }}">S'inscrire</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- End Navigation -->
-        <div class="clearfix"></div>
-    </header>
-    <!--header end-->
+    @include('partials.frontoffice.navbar')
 
     <!--banner start-->
     <section class="ImageBackground Blurb Blurb--wrapper bg-primary bg-primary--gradient310 u-BorderRadius6 js-FullHeight js-Parallax" data-overlay="4">
