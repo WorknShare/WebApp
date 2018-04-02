0f5c07cd581291582fd0b26f49907508e8be5e7b
SystemGlitch <jeremgamer@gmail.com>
2018-04-02 17:01:40 +0200

    Fixed site test when a site name or address contains html entities

--- database/factories/SiteFactory.php
+++ database/factories/SiteFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Site::class, function (Faker $faker) {
     return [
+        //'name' => $faker->unique()->streetName,
         'name' => $faker->unique()->streetName,
         'address' => $faker->unique()->address,
         'wifi' => $faker->boolean,
