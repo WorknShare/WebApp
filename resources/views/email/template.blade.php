3b25bb0114cab8fa6819281ae1339fc19c7536af
SystemGlitch <jeremgamer@gmail.com>
2018-04-30 11:50:15 +0200

    Improved email template

--- resources/views/email/template.blade.php
+++ resources/views/email/template.blade.php
@@ -6,18 +6,24 @@
     <title>@yield('title', 'Work\'n Share')</title>
 
 </head>
-<body style='font-family: "Open Sans","Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Helvetica Neue",sans-serif; font-size: 1rem; line-height: 1.4; background-color: #222222'>
+<body style='font-family: "Open Sans","Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Helvetica Neue",sans-serif; font-size: 1rem; line-height: 1.4; margin:0' background="#fff">
 
-	<table align="center" style="background-color: #ffffff; border: 1px solid #555" cellpadding="25px" cellspacing="0px">
+	<table align="center" cellpadding="25px" cellspacing="0px" width="100%">
 		<thead style="background-color: #39acd8;">
 			<tr>
 				<th>
-					<a href="{{ route('welcome') }}">
-		          		<img src="{{ asset('img/banner64_2.png') }}" class="logo logo-display" alt="">
-		      		</a>
+					<font color="#fff">
+						<a href="{{ route('welcome') }}" style="color:#fff">
+			          		<img src="{{ asset('img/banner64_2.png') }}" class="logo logo-display" alt="Work'n Share">
+			      		</a>
+		      		</font>
 		  		</th>
 	  		</tr>
   		</thead>
+
+	</table>
+
+	<table align="center" cellpadding="25px" cellspacing="0px" background="#fff">
   		<tbody>
   			<tr><td><h1>@yield('mailTitle')</h1></td>
 	  		<tr><td>@yield('content')</td></tr>
